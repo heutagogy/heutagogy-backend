5850cb9a84c7edf86b868c88d6a9efdecb36e496
Alexey Shmalko <rasen.dubi@gmail.com>
2016-12-06 00:58:08 +0200

    Pack heutagogy into setuptools

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(name='Heutagogy',
+      version='1.0',
+      url='https://github.com/heutagogy/heutagogy-backend',
+      packages=['heutagogy'],
+      license='AGPL3',
+      install_requires=[
+          'Flask',
+          'Flask-Login',
+      ],
+)
