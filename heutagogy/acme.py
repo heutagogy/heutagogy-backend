b23fffe1c2b8dce6ccc6570d3da22c0ee5aeecb4
Alexey Shmalko <rasen.dubi@gmail.com>
2017-02-26 20:03:08 +0200

    Revert "Add ACME response"

--- heutagogy/acme.py
+++
@@ -1,6 +0,0 @@
-from heutagogy import app
-
-if app.config['ACME_CHALLENGE']:
-    @app.route('/.well-known/acme-challenge/' + app.config['ACME_CHALLENGE'])
-    def get_acme():
-        return app.config['ACME_RESPONSE']
